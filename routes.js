83fecc53756dcb50a738e7f0671840209917f4e1
Justin Dallaire <dallaire.ju0@gmail.com>
2025-02-22 19:39:23 -0500

    correction d'une erreur dans le commit precedent

--- routes.js
+++ routes.js
@@ -1,8 +1,21 @@
 import { Router } from "express";
+import { supprimerTodo } from "./model/todo.js";
 
 const router = Router();
 
 //Definition des routes
 /*  ............. */
+
+router.delete("/api/todo/:id", (request, response) => {
+  const { id } = request.params;
+  try {
+    const todo = supprimerTodo(parseInt(id));
+    if (!todo) {
+      response.status(200).json({ note, message: "Todo supprimer" });
+    }
+  } catch (error) {
+    response.status(400).json({ error: error.message });
+  }
+});
 
 export default router;
