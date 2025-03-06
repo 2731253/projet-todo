a0f2a9f799de974260dd1f18bbd23a739572546f
EuniceMuleka <Eunice@gmail.com>
2025-03-06 12:52:45 -0500

    Création e la fonction pour la mise a jour d'une tâche

--- routes.js
+++ routes.js
@@ -5,4 +5,23 @@ const router = Router();
 //Definition des routes
 /*  ............. */
 
+//Mise a jour d'une tâche
+export const updateTodo = async (id) => {
+    const todo = await prisma.todo.findUnique({
+        where: {
+            id,
+        },
+    });
+ 
+    const todoUpdated = await prisma.todo.update({
+        where: {
+            id,
+        },
+        data: {
+            est_faite: !todo.est_faite,
+        },
+    });
+ 
+    return todoUpdated;
+};
 export default router;
